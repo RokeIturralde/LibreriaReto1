83b353a33c7741a3d85321ee97baf49bffd438ef
RokeIturralde <rokeitumendi@gmail.com>
2022-11-04 13:28:15 +0100

    added new values to the ResponseType enum

--- src/logic/objects/message/types/ResponseType.java
+++ src/logic/objects/message/types/ResponseType.java
@@ -1,11 +1,12 @@
 package logic.objects.message.types;
 
-public enum ResponseType{
+public enum ResponseType {
     OK,
     LOGIN_CREDENTIAL_ERROR,
     SERVER_ERROR,
     DATABASE_ERROR,
     USER_ERROR,
     EMAIL_EXISTS_ERROR,
-    LOGIN_EXISTS_ERROR
+    LOGIN_EXISTS_ERROR,
+    NOT_REGISTERED_ERROR
 }
