7b165580818c6ceaa7011417816b6371e1f9d579
RokeIturralde <rokeitumendi@gmail.com>
2022-11-08 09:00:36 +0100

    added new exception

---
+++ src/except/LoginCredentialException.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package except;
+
+/**
+ *
+ * @author 2dam
+ */
+public class LoginCredentialException extends Exception {
+
+    /**
+     * Creates a new instance of <code>LoginCredentialException</code> without
+     * detail message.
+     */
+    public LoginCredentialException() {
+        super("This User doesn't exist in our database, try to sign up first");
+    }
+
+    /**
+     * Constructs an instance of <code>LoginCredentialException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public LoginCredentialException(String msg) {
+        super(msg);
+    }
+}
